88a7c2467ee16d759b1420ca0d27824dd123e20a
oldTimer <1316767658@qq.com>
2023-10-31 17:23:56 +0800

    ts提示初始化

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly APP_BASE_API: string;
+  // 更多环境变量...
+}
+
+declare module "*.vue" {
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
